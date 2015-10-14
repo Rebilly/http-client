353ce8229d2379136f1cbf361b079a923f78eae0
slavcodev <slavcopost@gmail.com>
2015-10-14 05:29:15 +0300

    Add Adapter interface

---
+++ src/Adapter.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the HTTP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://rebilly.com
+ */
+
+namespace Rebilly\HttpClient;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+/**
+ * Interface Adapter.
+ *
+ * @author Veaceslav Medvedev <veaceslav.medvedev@rebilly.com>
+ * @version 0.1
+ */
+interface Adapter
+{
+    /**
+     * @param Request $request
+     * @param Response $response
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response);
+}
