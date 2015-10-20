a597a04a1329d32f58e573b9fda3d00e495229c9
slavcodev <slavcopost@gmail.com>
2015-10-20 23:46:53 +0300

    Add interface ClientException

---
+++ src/ClientException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the HTTP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://rebilly.com
+ */
+
+namespace Rebilly\HttpClient;
+
+/**
+ * Interface ClientException.
+ *
+ * @author Veaceslav Medvedev <veaceslav.medvedev@rebilly.com>
+ */
+interface ClientException
+{
+}
